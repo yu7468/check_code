f16ff750389a12b87154971e229bf0b7a633ab3b
yu7468 <cherish7468@gmail.com>
2024-09-28 17:45:53 +0900

    update

--- samuraitravel/src/main/java/com/example/samuraitravel/controller/FavoriteController.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/controller/FavoriteController.java
@@ -1,14 +1,23 @@
 package com.example.samuraitravel.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.samuraitravel.entity.Favorite;
 import com.example.samuraitravel.entity.House;
 import com.example.samuraitravel.entity.User;
 import com.example.samuraitravel.repository.FavoriteRepository;
 import com.example.samuraitravel.repository.HouseRepository;
 import com.example.samuraitravel.repository.UserRepository;
+import com.example.samuraitravel.security.UserDetailsImpl;
 import com.example.samuraitravel.service.FavoriteService;
 
 public class FavoriteController {
@@ -23,6 +32,16 @@ public class FavoriteController {
         this.favoriteRepository = favoriteRepository;
         this.favoriteService = favoriteService;
 	}
+	
+	@GetMapping("/favorites")
+    public String index(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl, @PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable, Model model) {
+        User user = userDetailsImpl.getUser();
+        Page<Favorite> favoritePage = favoriteRepository.findByUser(user,pageable);
+        
+        model.addAttribute("favoritePage", favoritePage);
+
+        return "favorites/index";
+    } 
 	
 	@PostMapping("/favorites/add")
     public String addFavorite(@RequestParam Integer houseId, @RequestParam Integer userId, RedirectAttributes redirectAttributes) {
